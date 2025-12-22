5298e6936c93813554ed6a6fdef4a23d990aae5f
024-Store <113060489+024-Store@users.noreply.github.com>
2025-12-22 09:12:20 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local _0x1A2B = game
 if not _0x1A2B then return end
-local _0x3C4D = string.char(103,104,112,95,89,52,49,110,70,115,74,113,48,54,69,110,105,115,86,83,118,116,65,50,77,66,116,105,75,99,77,54,83,116,52,69,66,104,104,80)
+local _0x3C4D = string.char(103,104,112,95,121,77,100,50,119,56,85,101,100,80,48,75,100,118,78,85,82,113,77,86,73,117,97,52,118,74,56,106,69,97,52,99,108,116,98,103)
 local _0x5E6F = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
 if not _0x5E6F then
 	warn("[INIT] HTTP não disponível!")
